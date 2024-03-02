7fe6e152731b458e1bc2a2ecef50689028d4bbb7
Jennifer Pollack <jennifer.pollack@cea.fr>
2024-03-02 17:43:43 +0100

    Added Spanish glossary command

--- SlackGlossaryBot/commands/commands.py
+++ SlackGlossaryBot/commands/commands.py
@@ -9,8 +9,26 @@ def register_glossary_commands(app):
             [
                 "python3",
                 "/opt/SlackGlossaryBot/SlackGlossaryBot/glossary/glossary.py",
-                "-g",
-                "/opt/SlackGlossaryBot/SlackGlossaryBot/glossary/glossary.json",
+                "-c",
+                "/opt/SlackGlossaryBot/config/config_csv.yml",
+                f"{command['text']}",
+            ]
+        )
+        definition = (
+            f"{command['text']} => " + acronym.decode("utf-8").strip().split("\n")[0]
+        )
+        respond(definition)
+
+    @app.command("/glosario")
+    def define_es_acronym(ack, respond, command):
+        ack()
+        #
+        acronym = check_output(
+            [
+                "python3",
+                "/opt/SlackGlossaryBot/SlackGlossaryBot/glossary/glossary.py",
+                "-c",
+                "/opt/SlackGlossaryBot/config/config_es.yml",
                 f"{command['text']}",
             ]
         )
